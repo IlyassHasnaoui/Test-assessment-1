516639f93867b4d242a49ed8c9150234321c3093
“Ilyass <ilyass.has@live.fr>
2023-09-15 14:47:06 -0400

    Updated arrays answers

--- arrays.js
+++ arrays.js
@@ -5,11 +5,16 @@ const faveColors = ['red', 'green', 'black']
 
 //CODE HERE
 
+colorCopy = faveColors.slice()
+console.log(colorCopy)
+
+
 //////////////////PROBLEM 2////////////////////
 /* Add a fourth color to the end of the 'colorCopy' array using the push method. */
 
 //CODE HERE
-
+colorCopy.push('White')
+console.log(colorCopy)
 //////////////////PROBLEM 3////////////////////
 /*
   Using an array method, create a new array called 'middleNums' from the 
@@ -19,7 +24,9 @@ const faveColors = ['red', 'green', 'black']
 const numbers = [1, 2, 3, 4, 5]
 
 //CODE HERE
-
+let middleNums = []
+middleNums.push(2, 3, 4)
+console.log(middleNums)
 
 //////////////////PROBLEM 4////////////////////
 /* 
@@ -34,3 +41,12 @@ const numbers = [1, 2, 3, 4, 5]
 const bigOrSmallArray = [1, 99, 42, 69, 102, -10, 159, 352]
 
 // CODE HERE
+let answers = []
+for (let i = 0; i < bigOrSmallArray.length; i++){
+  if (bigOrSmallArray[i] > 100){
+  answers.push('big')
+} else { (bigOrSmallArray <= 100)
+  answers.push('small')
+}
+}
+
